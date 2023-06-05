4738cf6f870aa71e90ee3bf2294c5ec67b8c498d
Thanh Nguyen <puffle.nnt@gmail.com>
2023-06-05 08:32:46 +0700

    Init project and CloudFormation template

---
+++ apps/api/dist/hello.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const hello = () => {
+    console.log("Hello World");
+    return "hello";
+};
+exports.hello = hello;
+//# sourceMappingURL=hello.js.map
